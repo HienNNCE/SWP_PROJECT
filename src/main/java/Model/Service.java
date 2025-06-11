deee5d3e4c935b2178ec5ae78b22c01f95adeba8
Nguyen Hien <pockethgaming@gmail.com>
2025-06-11 10:52:29 +0700

    add service list page and dao

--- src/main/java/Model/Service.java
+++ src/main/java/Model/Service.java
@@ -21,4 +21,53 @@ public class Service {
     private String serviceImg;
 
     // Constructor, getters, setters
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+
+    public void setServiceDescription(String serviceDescription) {
+        this.serviceDescription = serviceDescription;
+    }
+
+    public BigDecimal getServicePrice() {
+        return servicePrice;
+    }
+
+    public void setServicePrice(BigDecimal servicePrice) {
+        this.servicePrice = servicePrice;
+    }
+
+    public LocalDateTime getEstimateTime() {
+        return estimateTime;
+    }
+
+    public void setEstimateTime(LocalDateTime estimateTime) {
+        this.estimateTime = estimateTime;
+    }
+
+    public String getServiceImg() {
+        return serviceImg;
+    }
+
+    public void setServiceImg(String serviceImg) {
+        this.serviceImg = serviceImg;
+    }
+    
 }
