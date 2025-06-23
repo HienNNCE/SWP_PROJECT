c718782aee5170aff6f75b57ed3eab4ca503ee45
nvp10062003 <phucnvce170116@fpt.edu.vn>
2025-06-23 08:47:32 +0700

    phuc - update login for admin vs staff

--- src/main/java/DB/DBContext.java
+++ src/main/java/DB/DBContext.java
@@ -83,8 +83,8 @@ public class DBContext {
 
     // Database config (modify if needed)
     private static final String DB_URL = "jdbc:sqlserver://localhost:1433;databaseName=DriveXO;encrypt=true;trustServerCertificate=true";
-    private static final String DB_USER = "SA";
-    private static final String DB_PWD = "05102004@aZ09";
+    private static final String DB_USER = "sa";
+    private static final String DB_PWD = "1006";
 
     // Get a new connection each time
     public Connection getConnection() throws SQLException {
