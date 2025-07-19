18ba22a632a5ee4ec312c9c236c97a05d15a63d9
nhatthien483 <thiennhnce180035@fpt.edu.vn>
2025-07-19 20:24:24 +0700

    Add admin filter, staff filter and HashMD5 password

--- src/main/java/Controller/RegisterServlet.java
+++ src/main/java/Controller/RegisterServlet.java
@@ -8,6 +8,8 @@ import DAO.AuthenticationDAO;
 import Model.Users;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,72 +24,28 @@ import jakarta.servlet.http.HttpSession;
  * @author
  */
 public class RegisterServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet RegisterServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet RegisterServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method. This method is invoked when
-     * the registration form is submitted. It retrieves user input, performs
-     * validation, and attempts to register the user.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //processRequest(request, response);
+        // processRequest(request, response);
         AuthenticationDAO authenDao = new AuthenticationDAO();
         HttpSession session = request.getSession();
         // Retrieve parameters from the registration form.
-        String username = request.getParameter("username");
+        String fullName = request.getParameter("fullname");
         String email = request.getParameter("email");
+        String userName = request.getParameter("username");
         String password = request.getParameter("password");
         String confirm = request.getParameter("confirm");
+        String phone = request.getParameter("phone");
+        String address = request.getParameter("address");
+        String genderParam = request.getParameter("gender");
+        boolean gender = Boolean.parseBoolean(genderParam);
+        String dobParam = request.getParameter("dob");
+        LocalDate dob = LocalDate.parse(dobParam);
+        String aboutMe = request.getParameter("aboutMe");
 
         String passwordPattern = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$";
-        
+
         if (!password.equals(confirm)) {
             request.setAttribute("err", "Passwords do not match.");
             request.getRequestDispatcher("register.jsp").forward(request, response);
@@ -95,7 +53,8 @@ public class RegisterServlet extends HttpServlet {
         }
 
         if (!password.matches(passwordPattern)) {
-            request.setAttribute("err", "Password must be at least 8 characters long and contain a mix of letters, numbers, and symbols.");
+            request.setAttribute("err",
+                    "Password must be at least 8 characters long and contain a mix of letters, numbers, and symbols.");
             request.getRequestDispatcher("register.jsp").forward(request, response);
             return;
         }
@@ -105,12 +64,11 @@ public class RegisterServlet extends HttpServlet {
             request.setAttribute("err", "This email has already been registered.");
             request.getRequestDispatcher("register.jsp").forward(request, response);
         } else {
-            authenDao.registerUser(username, email, password);
+            authenDao.registerUser(userName, email, password, phone, fullName, gender, dob, aboutMe, address);
             request.setAttribute("success_msg", "Registration successful! Please log in.");
             request.getRequestDispatcher("login.jsp").forward(request, response);
         }
     }
-    
 
     /**
      * Returns a short description of the servlet.
